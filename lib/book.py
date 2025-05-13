8b6f6fb8f2d5afae04870fe53e764596f1e6d633
JoyNjoroge <njorogejoy78@gmail.com>
2025-05-13 21:20:20 +0300

    Final Commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    # first test
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+    # testing it
+# book = Book("And Then There Were None", 272)
+# print(book.title)
+# print(book.page_count)
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance (value, int):
+            print("page_count must be an integer")
+        self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    # first test
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Old"
+# test if worked 
+# stan_smith = Shoe("Adidas", 9) 
+# print(stan_smith.brand)
+# print(stan_smith.size)
+
+    # second test
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, value):
+        # shoesize should be int
+        if not isinstance (value, int) :
+            print("size must be an integer")
+        self._size = value
+
+# s = Shoe("Nike", "large")
+# print(s.size)
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+stan_smith = Shoe("Adidas", 9)
+stan_smith.cobble()
+assert(stan_smith.condition == "New")
